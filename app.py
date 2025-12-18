b2969f4fa8c85cf3bbfc81538a40daa37a9f0b94
shafaana6 <khaalidashafaana@gmail.com>
2025-12-18 16:40:39 +0530

    Test Railway MySQL connection

--- app.py
+++ app.py
@@ -1,10 +1,16 @@
 import os
 import mysql.connector
 
-conn = mysql.connector.connect(
-    host=os.getenv("MYSQLHOST"),
-    user=os.getenv("MYSQLUSER"),
-    password=os.getenv("MYSQLPASSWORD"),
-    database=os.getenv("MYSQLDATABASE"),
-    port=int(os.getenv("MYSQLPORT"))
-)
+try:
+    conn = mysql.connector.connect(
+        host=os.getenv("MYSQLHOST"),
+        user=os.getenv("MYSQLUSER"),
+        password=os.getenv("MYSQLPASSWORD"),
+        database=os.getenv("MYSQLDATABASE"),
+        port=int(os.getenv("MYSQLPORT"))
+    )
+    print("✅ Database connected successfully")
+
+except Exception as e:
+    print("❌ Database connection failed")
+    print(e)
